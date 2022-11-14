ef83be9cd3809582a22a7cf7d47ed57771f3338f
WGS <53532339+GuangShuaiWang@users.noreply.github.com>
2022-11-14 10:17:20 +0800

    add main programm

---
+++ Python/TLimmuno2.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import argparse
+import os
+os.chdir(os.getcwd())
+
+#load model
+BA_model = tf.keras.models.load_model("./Python/model/BAmodel")
+BAmodel = tf.keras.models.Model(inputs = BA_model.input,outputs = BA_model.layers[-2].output)
+TLimmuno2 = tf.keras.models.load_model("./Python/model/TLimmuno2")
+pseudo_seq = pd.read_feather("./data/pseudo_blosum62.feather")
+pseudo_seq_file = pd.read_table("./data/pseudosequence.2016.all.X.dat",header=None,names=("HLA","sequence"))
+def blosum62(peptide,maxlen):
+
+    Blosum62_matrix = pd.read_csv("./data/BLOSUM62.csv",comment="#")
+    Protein_alphabet = list("ARNDCQEGHILKMFPSTWYVX")
+    Blosum62_matrix = Blosum62_matrix[Protein_alphabet]
+    Blosum62_matrix = Blosum62_matrix.loc[Protein_alphabet]
+    encoder = np.empty((maxlen,21))
+    if len(peptide) <=maxlen:
+        peptide = peptide + "X"*(maxlen-len(peptide))
+    for i in range(len(peptide)):
+        pep = list(peptide)[i]
+        coder = Blosum62_matrix[pep]
+        encoder[i] = coder
+
+    return encoder.flatten()
+
+def data_process(args):
+    mode = args.mode
+
+    if mode == "line":
+        DF = pd.DataFrame({"pep":args.epitope,"HLA":args.hla})
+    if mode == "file":
+        DF = pd.read_csv("{}".format(args.intdir),header=None,names=("pep","HLA"))
+    DF = pd.merge(DF,pseudo_seq_file)
+    return DF
+
+def peptide_code(Data):
+    Data["pep_blosum"] = Data["pep"].apply(blosum62,args=(21,))
+    peptide = np.empty((len(Data),21,21))
+    for i in range(len(Data)):
+        peptide[i] = Data["pep_blosum"][i].reshape((21,21))
+    
+    return peptide
+
+def MHC_code(Data):
+    Data["MHC_blosum"] = Data["sequence"].apply(blosum62,args=(34,))
+    MHC = np.empty((len(Data),34,21))
+    for i in range(len(Data)):
+        MHC[i] = Data["MHC_blosum"][i].reshape((34,21))
+    return MHC
+
+def model_predict(peptide,MHC):
+    BA = BAmodel.predict([peptide,MHC])
+    IMM_result = TLimmuno2.predict([peptide,MHC,BA])
+    
+    return IMM_result
+
+
+
+
+def rank(Data):
+    IMM_bg_pep = pd.read_csv("./data/IMM_bg_pep.csv")
+    IMM_bg_pep["pep_blosum"] = IMM_bg_pep["pep"].apply(blosum62,args=(21,))
+    DF = pd.DataFrame()
+    for i in Data["HLA"].unique():
+        IMM_bg_pep["MHC"] = i
+        x = pd.merge(IMM_bg_pep,pseudo_seq[["MHC","MHC_blosum"]])
+        peptide = np.empty((len(x),21,21))
+        for z in range(len(x)):
+            peptide[z] = x["pep_blosum"][z].reshape((21,21))
+        MHC = np.empty((len(x),34,21))
+        for z in range(len(x)):
+            MHC[z] = x["MHC_blosum"][z].reshape((34,21))
+        BA = BAmodel.predict([peptide,MHC])
+        IMM_result = TLimmuno2.predict([peptide,MHC,BA])
+        IMM_result = IMM_result.tolist()
+        
+        y = Data[Data["HLA"]== i]
+        Rank = []
+        for I in y["prediction"].values:
+            IMM_result.append(I)
+            rank = 1-(sorted(IMM_result).index(IMM_result[-1])+1)/90001
+            Rank.append(rank)
+            IMM_result.pop()
+        y["Rank"] = Rank
+        DF = pd.concat([DF,y])
+        DF.pop("MHC_blosum")
+        DF.pop("pep_blosum")
+
+        return DF
+
+
+def main(args):
+    Data = data_process(args)
+    peptide = peptide_code(Data)
+    MHC = MHC_code(Data)
+    prediction_result = model_predict(peptide,MHC)
+    Data["prediction"] = prediction_result
+    Result = rank(Data)
+    if args.model == "line":
+        print("epitope score : {}".format(Result["prediction"]))
+        print("epitope rank : {}".format(Result["Rank"]))
+    if args.model == "file":
+        Result.to_csv("{}".format(args.outdir))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='TLimmuno2 command line')
+    parser.add_argument('--mode',type=str,default='file',help='line mode or file mode')
+    parser.add_argument('--epitope',type=str,default=None,help='if line mode, specifying your epitope')
+    parser.add_argument('--hla',type=str,default=None,help='if line mode, specifying your HLA allele')
+    parser.add_argument('--intdir',type=str,default=None,help='if file mode, specifying the path to your input file')
+    parser.add_argument('--outdir',type=str,default=None,help='if file mode, specifying the path to your output folder')
+    args = parser.parse_args()
+    main(args)
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# TLimmuno2
-TLimmuno2: predicting HLA class II antigen immunogenicity through integrated deep learning
+# TLimmuno2: predicting HLA class II antigen immunogenicity through integrated deep learning
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![version](https://img.shields.io/badge/version-dev-green.svg)](https://shields.io/)
+
+## Overview
+
+This repository provides the analysis reports, code, data and python packages for readers who are interest in this project and make it easier to reproduce the whole analysis procedure.
+
+Read online analysis report at https://xsliulab.github.io/TLimmuno2/.
+
+## Contents
+
+* [Python](./Python) The python code of TLimmuno2 package.
+* [data](./data) The data used and produced by analysis report.
+* [docs](./docs) Website pages and figures used for showing analysis reports.
+* [report](./report) Rmarkdown files of analysis report and related html web page files.
+* [figure](./figure) The figure produced by all Rmarkdown files.
+
+## how to use
+
+Run example
+
+
+## Citation
+
+need to add paper
+
+## Acknowledgement
+
+We thank ShanghaiTech University High Performance Computing Public Service Platform for computing services.This work was supported by Shanghai Science and Technology Commission (21ZR1442400), the National Natural Science Foundation of China (31771373), and startup funding from ShanghaiTech University.
+
+## License
+
+***
+
+**Cancer Biology Group @ShanghaiTech**
+
+**Research group led by Xue-Song Liu in ShanghaiTech University**
+
+
